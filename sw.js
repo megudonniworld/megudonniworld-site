3616fd447240490ffea467d355d48bd330a6a32b
megudonniworld <megudonni@gmail.com>
2025-08-11 19:38:12 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+// very minimal service worker
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', () => { /* optional: add caching later */ });
